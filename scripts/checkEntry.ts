59d787d9ef36d7bd0ce48eeac03d2e0c8c4357db
juuroudojo <105825252+juuroudojo@users.noreply.github.com>
2023-08-29 18:00:47 +0300

    m

--- scripts/checkEntry.ts
+++ scripts/checkEntry.ts
@@ -5,7 +5,7 @@ async function main() {
     // Creating an interactable contract variable.
     // "ContractName" is the Contract/Interface of the contract we want to call (We need to have either the full code or interface compiled)
     //  contractaddr - address of the contract to be called
-    const kyc = await ethers.getContractAt("KYCTest", "0x40372407a74de28BEe1Cd51c809d56aCA3c871a2")
+    const kyc = await ethers.getContractAt("KYCTest", "0x49500cBaEA9c97717373718c5a5e2170F0Dad34f")
     let entered = await kyc.hasEntered("0xced8ec16e2b4964a834ed996a6d85aa007858bda");
 
     console.log(entered);
