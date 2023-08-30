c7d9aba74f6ac76c07daebd1fd3f2f2866be07c1
juuroudojo <105825252+juuroudojo@users.noreply.github.com>
2023-08-30 14:24:34 +0300

    m

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,53 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Learn more about ERC721 standard: https://docs.openzeppelin.com/contracts/3.x/erc721
+contract NFT721 is ERC721URIStorage, Ownable {
+    constructor() ERC721("MyNFT", "NFT") {}
 
-contract NFT721 is ERC721, AccessControl, Ownable {
-    uint256 tokenCounter;
-    
-    mapping(uint256 => string) private _URIs;
-
-    constructor() ERC721("TestCollection", "TCL") {
-        // needed to allign with ids (ids start with 1, while tokenCounter is 0). Further on Counters library will be used instead.
-        tokenCounter = 1;
-        // AccessControl library function. Grants a role to the deployer of the contract. 
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    }
-
-    function mint(address _to, string memory _tokenURI)
-        public
-        onlyRole(DEFAULT_ADMIN_ROLE)
-        returns (uint256)
-    {
-        uint256 newItemId = tokenCounter;
-        _safeMint(_to, newItemId);
-        _setTokenURI(newItemId, _tokenURI);
-        tokenCounter = tokenCounter + 1;
-
-        return newItemId;
-    }
-
-    function _setTokenURI(uint256 id, string memory _tokenURI) internal {
-        _URIs[id] = _tokenURI;
-    }
-
-    function tokenURI(uint256 id) public view override returns (string memory) {
-        return _URIs[id];
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(AccessControl, ERC721)
-        returns (bool)
-    {
-        return
-            interfaceId == type(IAccessControl).interfaceId ||
-            super.supportsInterface(interfaceId);
+    function mint(address to, uint256 tokenId, string memory tokenURI) external onlyOwner {
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
     }
 }
---
+++ scripts/mint.ts
@@ -0,0 +1,22 @@
+import { BigNumber } from "ethers";
+import {ethers} from "hardhat"
+
+async function main() {
+    // Creating an interactable contract variable.
+    // "ContractName" is the Contract/Interface of the contract we want to call (We need to have either the full code or interface compiled)
+    //  contractaddr - address of the contract to be called
+    const deployedNFT = await ethers.getContractAt("NFT721", "0x2B15f32dB5114Fe181443540597849bFa33ff5cC")
+    // let amountMinted = await deployedToken.mint(addr, amount) as BigNumber;
+
+    // console.log("You have successfully minted", amountMinted)
+    await deployedNFT.mint("0xced8ec16e2b4964a834ed996a6d85aa007858bda", 1, "sampleurl");
+
+    console.log("Success")!
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
