f1b130bc1b57477cb0afb1dda10a355a89ba69fc
juuroudojo <105825252+juuroudojo@users.noreply.github.com>
2024-01-07 19:27:33 +0200

    Frontend

--- front-end/smart-contracts/contracts/SendCAM.sol
+++ front-end/smart-contracts/contracts/SendCAM.sol
@@ -2,11 +2,39 @@
 pragma solidity ^0.8.9;
 
 contract SendCAM {
-    function transfer(address payable _to, uint256 _amount) external {
+    uint256 transactionCounter;
+
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+    }
+
+    TransferStruct[] transactions;
+
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp);
+
+    function transfer(address payable _to, string memory _message) external payable {
         // A preferred way of transferring native currency compared to `.send`
         // Reason for that is gas limit that introduces oportunities for reentrancy (Refer to challanges)
-        (bool sent, ) = _to.call{value: _amount}("");
+        (bool sent, ) = _to.call{value: msg.value}("");
         
         require(sent, "Failed to send CAM");
+
+        transactionCounter++;
+
+        transactions.push(TransferStruct(msg.sender, _to, msg.value, _message, block.timestamp));
+
+        emit Transfer (msg.sender, _to, msg.value, _message, block.timestamp);
+    }
+
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCounter;
     }
 }
--- front-end/smart-contracts/hardhat.config.ts
+++ front-end/smart-contracts/hardhat.config.ts
@@ -1,8 +1,24 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import '@openzeppelin/hardhat-upgrades';
+
+dotenv.config();
+
+
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    columbus: {
+      url: process.env.COLUMBUS_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
