1c0fbc416ddc7e2b745acba59dfabc0c375b49c8
Athanasios Katsios <aka@anixe.io>
2024-01-27 12:43:12 +0200

    Created first token

--- token/contracts/Token.sol
+++ token/contracts/Token.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract TestToken is ERC20, AccessControl {
     address public owner;
 
-    constructor(uint256 initialSupply) ERC20("Earth", "EAR") {
+    constructor(uint256 initialSupply) ERC20("Sustainability Reward Token", "SRT") {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _mint(msg.sender, initialSupply);
 
